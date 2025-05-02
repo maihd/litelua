bf9ec82650f1c3374c3c7cba0fc07750103f892a
maihd.dev <maihd.dev@gmail.com>
2025-05-02 15:35:00 +0700

    build_common.bat

--- build_common.bat
+++ build_common.bat
@@ -1,9 +1,6 @@
 @echo off
 
-set LITELUA_DIR=%~dp0
-
-set LITELUA_OUTPUT=%LITELUA_DIR%\output
-set LITELUA_BUILD_DIR=%LITELUA_DIR%\build
+call build_vars.bat
 
 if exist %LITELUA_BUILD_DIR% (
     rmdir /S /Q %LITELUA_BUILD_DIR%
